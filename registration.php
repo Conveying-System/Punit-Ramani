541a76175f0a4153329f48659e59c83f632011ac
punitramani <46717931+punitramani@users.noreply.github.com>
2020-04-05 16:53:38 +0530

    connected with server

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+$status=0;
+$n=$_POST["emailid"];
+$c=$_POST["name"];
+$b=$_POST["pwd"];
+$k=$_POST["cnf"];
+$con=mysqli_connect("localhost","root","","travel");
+if(!$con)
+die("Server could not connected");
+if($b==$k){
+$sqldata="insert into customer(UserName,Name,Password) values('".$n."','".$c."','".$b."')";
+}
+else
+{
+    die("password incorrect");
+}
