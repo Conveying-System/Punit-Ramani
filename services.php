a5b3d268cf53474d679d22d9d4e90ffe0e4af6a3
punitramani <46717931+punitramani@users.noreply.github.com>
2020-04-05 17:09:54 +0530

    frontend

---
+++ services.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="bootstrap3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<div class="container">
+<img src="high2.jpg" height=800 width=100% style=border-radius:8px;opacity:0.8;>
+<div class="topleft"><span class="ex1">T</span>RANS</div>
+<div class="topright">
